142cd601129eed4b8657cbda2c0d39af254c63b4
DiazFede <diazfedericoucu@gmail.com>
2024-09-23 10:11:54 -0300

    A

--- TA17/src/App.jsx
+++ TA17/src/App.jsx
@@ -1,10 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Product from './components/Product';
 import { Home, About, Contact } from './components/Route';
 
 const App = () => {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/product/1">Product 1</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
